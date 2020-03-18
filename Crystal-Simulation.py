b5b5c2de261546ce2e052493d3c7bcf66f064a55
MaxCantCode <35848843+MaxCantCode@users.noreply.github.com>
2020-03-18 19:26:42 -0400

    Update Crystal-Simulation.py

--- Crystal-Simulation.py
+++ Crystal-Simulation.py
@@ -17,37 +17,31 @@ def voronoi_volumes(points):
     return vol
 
 
-# giant cells
-points = np.array([[0, 0],
-                   [0, 2],
-                   # [0, -2],
-                   [2, 0],
-                   [-2, 0],
-                   [0, 1],
-                   # [0, -1],
-                   [1, 0 - 1 / 25],
-                   [-1, 0]])
-voronoi_plot_2d(Voronoi(points))
-plt.title('giant cells\n{}'.format(voronoi_volumes(points)))
-plt.show()
+if True:  # toggle for giant demo
+    # giant cells
+    points = np.array([[0, 0],
+                       [0, 1],
+                       [0, 1 / 2],
+                       [1, -0.4],
+                       [-1, 0]])
+    voronoi_plot_2d(Voronoi(points))
+    plt.title('giant cells\n{}'.format(voronoi_volumes(points)))
+    plt.show()
 
-# normal example
-points = np.array([[0, 0],
-                   [0, 2],
-                   [0, -2],
-                   [2, 0],
-                   [-2, 0],
-                   [0, 1],
-                   [0, -1],
-                   [1, 0],
-                   [-1, 0]])
-voronoi_plot_2d(Voronoi(points))
-plt.title('normal example\n{}'.format(voronoi_volumes(points)))
-plt.show()
+    # normal example
+    points = np.array([[0, 0],
+                       [0, 1],
+                       [0, 1 / 2],
+                       [1, 0],
+                       [-1, 0]])
+    voronoi_plot_2d(Voronoi(points))
+    plt.title('normal example\n{}'.format(voronoi_volumes(points)))
+    plt.show()
 
 np.random.seed(370)
 # num_pts = 10 ** 5
 num_pts = 10 ** 2 // 2
+# num_pts = 15
 points = np.random.rand(num_pts, 2)
 
 volumes_ = np.sort(voronoi_volumes(points))
@@ -63,31 +57,45 @@ num_biggr = np.count_nonzero(cum_no_inf > 1)
 # look around above/below 1, choose whichever closer
 crit_pts = cum_no_inf[-num_biggr - 1:-num_biggr + 2]
 
+if True:  # for num_pts=15<20
+    try:
+        np.argmin(abs(crit_pts - 1))
+    except ValueError as e:
+        print(e)  # not many giants
+        crit_pts = cum_no_inf[-num_biggr - 1:]
+
 print(crit_pts,
       cum_no_inf[-num_biggr],
       abs(crit_pts - 1),
       'argmin: {}'.format(np.argmin(abs(crit_pts - 1))),
-      'num_biggr: {}'.format(num_biggr),
+      'num of giant cells: {}'.format(num_biggr),
       sep='\n')
 
 # ab1 means above/below 1
 ab1 = np.argmin(abs(crit_pts - 1))
-volumes = v_no_inf[:-num_biggr + ab1]
-print(np.sum(v_no_inf))
-print(np.sum(volumes))
+# volumes = v_no_inf[:-num_biggr + ab1]
+volumes = v_no_inf if len(v_no_inf[:-num_biggr + ab1]) == 0 else v_no_inf[:-num_biggr + ab1]
 
-plt.title('Area of Each Cell (with giants)')
-plt.plot(volumes_, '+')
-plt.show()
+print('vol sum before:', np.sum(v_no_inf))
+print('vol sum after:', np.sum(volumes))
 
-plt.title('Area of Each Cell')
-plt.plot(volumes, '+')
-plt.show()
+if True:
+    plt.title('Volume of Each Cell (with giants)')
+    plt.plot(volumes_, '+')
+    plt.show()
 
-plt.title('cumsum Area of Each Cell')
-plt.plot(np.cumsum(volumes), '+')
-plt.show()
+    plt.title('Volume of Each Cell')
+    plt.plot(volumes, '+')
+    plt.show()
 
-plt.title('Hist of Area Plot')
-plt.hist(volumes)
-plt.show()
+    plt.title('cumsum Volume of Each Cell')
+    plt.plot(np.cumsum(volumes), '+')
+    plt.show()
+
+    plt.title('Hist of Volume Plot')
+    plt.hist(volumes)
+    plt.show()
+
+    plt.title(r'$F_n(s)=\frac{{n(s)}}{{n(s_{{max}})}},n_{{cells}}={}$'.format(len(volumes)))
+    plt.plot(volumes, np.linspace(0, 1, len(volumes)), '+')
+    plt.show()
